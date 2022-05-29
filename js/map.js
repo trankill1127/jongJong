f8cf222da07ea0061adababb4ee01e1adce0cc57
Jinnie Kim <jinniek48@gmail.com>
2022-05-30 08:13:39 +0900

    add campusData

--- js/map.js
+++ js/map.js
@@ -1,11 +1,24 @@
 var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
     
 mapOption = { 
-    //center: new kakao.maps.LatLng(37.551054, 127.073939), // 지도의 중심좌표(세종대)
-    center : new kakao.maps.LatLng(37.362830, 127.119538),
+    center: new kakao.maps.LatLng(37.551054, 127.073939), // 지도의 중심좌표(세종대)
+    //center : new kakao.maps.LatLng(37.362830, 127.119538),
     level: 3 // 지도의 확대 레벨
 };
 
 // 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
 var map = new kakao.maps.Map(mapContainer, mapOption); 
 
+function resizeMap() {
+    var mapContainer = document.getElementById('map');
+    mapContainer.style.width = '650px';
+    mapContainer.style.height = '650px'; 
+}
+
+function relayout() {    
+    
+    // 지도를 표시하는 div 크기를 변경한 이후 지도가 정상적으로 표출되지 않을 수도 있습니다
+    // 크기를 변경한 이후에는 반드시  map.relayout 함수를 호출해야 합니다 
+    // window의 resize 이벤트에 의한 크기변경은 map.relayout 함수가 자동으로 호출됩니다
+    map.relayout();
+}
--- js/mapping.js
+++ js/mapping.js
@@ -1,3 +1,6 @@
+var shortDis=null;
+var shortTime = null;
+
 class Node { //helper class for PriorityQueue
 
     constructor(val, priority) {
@@ -120,9 +123,16 @@ class WeightedGraph {
         // as long as there is something to visit
         while (nodes.values.length) { //우선순위 큐가 비어있지 않은 경우(=방문할 정점이 남아있는 경우)
 
-            smallest = nodes.dequeue().val; //가장 작은 ?을 가지는 우선순위 큐에서 뺴냄
+            smallest = nodes.dequeue().val; //가장 작은 거리을 가지는 우선순위 큐에서 뺴냄
 
             if (smallest === finish) {
+
+                shortDis=Math.round(distances[finish]);
+                shortTime = Math.round((shortDis/1.35/60));
+
+                console.log(shortDis+"m");
+                console.log("도보로 약"+shortTime+"분이 소요됩니다.");
+
                 //WE ARE DONE
                 //BUILD UP PATH TO RETURN AT END
                 while (previous[smallest]) {
@@ -158,12 +168,94 @@ class WeightedGraph {
   
 //정점, 위도, 경도 데이터
 var vertexData = [
-  ["집", 37.362830, 127.119538],
-  ["도서관", 37.358070, 127.116616],
-  ["마트", 37.365073, 127.114607],
-  ["초등학교", 37.364864, 127.113617],
-  ["놀이터", 37.366316, 127.111745],
-  ["역", 37.367670, 127.108387]
+  ["1", 37.552929, 127.072469],
+  ["2", 37.552120 ,127.072817],
+  ["3", 37.552757 ,127.073613],
+  ["4", 37.552642 ,127.073788],
+  ["5", 37.552436 ,127.073552],
+  ["6", 37.551919 ,127.073466],
+  ["7", 37.552182 ,127.073493],
+  ["8", 37.552223 ,127.073844],
+  ["9", 37.552070 ,127.073654],
+  ["10", 37.551944, 127.073823],
+  ["11", 37.551887 ,127.073128],
+  ["12", 37.552106 ,127.073995],
+  ["13", 37.551784 ,127.073648],
+  ["14", 37.551629 ,127.073450],
+  ["15", 37.551576 ,127.073910],
+  ["16", 37.551439 ,127.073688],
+  ["17", 37.551439 ,127.074092],
+  ["18", 37.551271 ,127.073904],
+  ["19", 37.551205 ,127.073987],
+  ["20", 37.551114 ,127.074102],
+  ["21", 37.551280 ,127.074302],
+  ["22", 37.551400 ,127.074457],
+  ["23", 37.551605 ,127.074698],
+  ["24", 37.551864 ,127.074932],
+  ["25", 37.551152 ,127.075312],
+  ["26", 37.550896 ,127.074987],
+  ["27", 37.550605 ,127.074601],
+  ["28", 37.550845 ,127.073532],
+  ["29", 37.551068 ,127.073349],
+  ["30", 37.551370 ,127.073768],
+  ["31", 37.550305 ,127.072844],
+  ["32", 37.549780 ,127.073428],
+  ["33", 37.549319 ,127.073471],
+  ["34", 37.550303 ,127.073309],
+  ["35", 37.550464 ,127.073514],
+  ["36", 37.550680 ,127.073725],
+  ["37", 37.550271 ,127.074207],
+  ["38", 37.550080 ,127.073585],
+  ["39", 37.550219 ,127.073791],
+  ["40", 37.550042 ,127.074007],
+  ["41", 37.549965 ,127.073702],
+  ["42", 37.550017 ,127.073814],
+  ["43", 37.549796 ,127.074073],
+  ["44", 37.549881 ,127.074177],
+  ["45", 37.549630 ,127.074110],
+  ["46", 37.549619 ,127.073987],
+  ["47", 37.549633 ,127.074347],
+  ["48", 37.549770 ,127.074299],
+  ["49", 37.550154 ,127.074114],
+  ["50", 37.549860 ,127.074427],
+  ["51", 37.549298 ,127.074113],
+  ["52", 37.549302 ,127.073370],
+  ["53", 37.548783 ,127.073330],
+  ["54", 37.548481 ,127.073341],
+  ["55", 37.548764 ,127.073716],
+  ["56", 37.548455 ,127.073703],
+  ["57", 37.548457 ,127.074039],
+  ["58", 37.548416 ,127.074027],
+  ["59", 37.548760 ,127.074068],
+  ["60", 37.548348 ,127.073351],
+  ["61", 37.548936 ,127.074083],
+  ["62", 37.549315 ,127.074406],
+  ["63", 67.549853 ,127.074876],
+  ["64", 37.549786 ,127.074896],
+  ["65", 37.549695 ,127.074672],
+  ["66", 37.549661 ,127.074639],
+  ["67", 37.549809 ,127.074968],
+  ["68", 37.549571 ,127.075005],
+  ["69", 37.549535 ,127.074966],
+  ["70", 37.549193 ,127.074773],
+  ["71", 37.548886 ,127.074644],
+  ["72", 37.549046 ,127.075229],
+  ["73", 37.550522 ,127.075807],
+  ["74", 37.550405 ,127.076028],
+  ["75", 37.550779 ,127.076178],
+  ["76", 37.550983 ,127.075886],
+  ["77", 37.551071 ,127.076302],
+  ["78", 37.551202 ,127.075918],
+  ["79", 37.551154 ,127.075856],
+  ["80", 37.550992 ,127.075127],
+  ["81", 37.550966 ,127.075162],
+  ["82",37.551037 ,127.075261],
+  ["83", 37.551022 ,127.075288],
+  ["84", 37.550798 ,127.075406],
+  ["85", 37.550834 ,127.075442],
+  ["86", 37.548890 ,127.075007],
+  ["87", 37.552038, 127.073313],
+  ["88", 37.550889, 127.073574]
 ]
 
 var polyline = new kakao.maps.Polyline({
@@ -187,29 +279,153 @@ function getShortCut(){
     var finishPos = null;
    
 
-    graph.addVertex("집"); //A
-    graph.addVertex("도서관"); //B
-    graph.addVertex("마트"); //C
-    graph.addVertex("초등학교"); //D
-    graph.addVertex("놀이터"); //E
-    graph.addVertex("역"); //F
+    graph.addVertex("1"); graph.addVertex("2"); graph.addVertex("3");graph.addVertex("4");graph.addVertex("5");
+    graph.addVertex("6"); graph.addVertex("7"); graph.addVertex("8");graph.addVertex("9");graph.addVertex("10");
+    graph.addVertex("11");graph.addVertex("12");graph.addVertex("13");graph.addVertex("14");graph.addVertex("15");
+    graph.addVertex("16");graph.addVertex("17");graph.addVertex("18");graph.addVertex("19");graph.addVertex("20");
+    graph.addVertex("21");graph.addVertex("22");graph.addVertex("23");graph.addVertex("24");graph.addVertex("25");
+    graph.addVertex("26");graph.addVertex("27");graph.addVertex("28");graph.addVertex("29");graph.addVertex("30");
+    graph.addVertex("31");graph.addVertex("32");graph.addVertex("33");graph.addVertex("34");graph.addVertex("35");
+    graph.addVertex("36");graph.addVertex("37");graph.addVertex("38");graph.addVertex("39");graph.addVertex("40");
+    graph.addVertex("41");graph.addVertex("42");graph.addVertex("43");graph.addVertex("44");graph.addVertex("45");
+    graph.addVertex("46");graph.addVertex("47");graph.addVertex("48");graph.addVertex("49");graph.addVertex("50");
+    graph.addVertex("51");graph.addVertex("52");graph.addVertex("53");graph.addVertex("54");graph.addVertex("55");
+    graph.addVertex("56");graph.addVertex("57");graph.addVertex("58");graph.addVertex("59");graph.addVertex("60");
+    graph.addVertex("61");graph.addVertex("62");graph.addVertex("63");graph.addVertex("64");graph.addVertex("65");
+    graph.addVertex("66");graph.addVertex("67");graph.addVertex("68");graph.addVertex("69");graph.addVertex("70");
+    graph.addVertex("71");graph.addVertex("72");graph.addVertex("73");graph.addVertex("74");graph.addVertex("75");
+    graph.addVertex("76");graph.addVertex("77");graph.addVertex("78");graph.addVertex("79");graph.addVertex("80");
+    graph.addVertex("81");graph.addVertex("82");graph.addVertex("83");graph.addVertex("84");graph.addVertex("85");
+    graph.addVertex("86");graph.addVertex("87");graph.addVertex("88");
     
-    graph.addEdge("집", "도서관", 4);
-    graph.addEdge("집", "마트", 2);
-    graph.addEdge("도서관", "놀이터", 3);
-    graph.addEdge("마트", "초등학교", 2);
-    graph.addEdge("마트", "역", 4);
-    graph.addEdge("초등학교", "놀이터", 3);
-    graph.addEdge("초등학교", "역", 1);
-    graph.addEdge("놀이터", "역", 1);
+    graph.addEdge("1", "2", 98.51);
+  graph.addEdge("2", "3", 99.87);
+  graph.addEdge("2", "11",38.72 );
+  graph.addEdge("3", "4", 20.14);
+  graph.addEdge("4", "5", 29.15);
+  graph.addEdge("4", "24",131.94 );
+  graph.addEdge("5", "7", 28.27 );
+  graph.addEdge("5", "8", 36.12);
+  graph.addEdge("87", "6",19.23 );
+  graph.addEdge("7", "87",24.53 );
+  graph.addEdge("6", "9",24.53 );
+  graph.addEdge("6", "13", 19.92);
+  graph.addEdge("87", "11",22.76 );
+  graph.addEdge("7", "9", 19.27);
+  graph.addEdge("9", "10", 19.92);
+  graph.addEdge("11", "14", 40.03);
+  graph.addEdge("9", "8", 24.77);
+  graph.addEdge("8", "12", 16.72);
+  graph.addEdge("12", "10",23.34 );
+  graph.addEdge("10", "13", 24.53);
+  graph.addEdge("13", "15",32.19 );
+  graph.addEdge("13", "14", 24.05);
+  graph.addEdge("14", "16", 29.11);
+  graph.addEdge("16", "15",23.49 );
+  graph.addEdge("16", "30",10.91 );
+  graph.addEdge("15", "17",22.96 );
+  graph.addEdge("30", "29", 48.86);
+  graph.addEdge("30", "18",15.43 );
+  graph.addEdge("17", "18",24.64 );
+  graph.addEdge("17", "21", 26.26);
+  graph.addEdge("29", "28", 31.18);
+  graph.addEdge("19", "88", 47.65);
+  graph.addEdge("88", "28",7.53 );
+  graph.addEdge("29", "88", 28.43);
+  graph.addEdge("18", "19", 11.20);
+  graph.addEdge("19", "20", 14.32);
+  graph.addEdge("20", "21",25.36 );
+  graph.addEdge("20", "27",71.32 );
+  graph.addEdge("21", "22", 18.94);
+  graph.addEdge("22", "23",43.39 );
+  graph.addEdge("22", "26", 70.87);
+  graph.addEdge("23", "24",31.23 );
+  graph.addEdge("23", "25", 72.27);
+  graph.addEdge("25", "26", 39.09);
+  graph.addEdge("26", "80", 17.77);
+  graph.addEdge("80", "27", 28.90);
+  graph.addEdge("80", "81",4.26 );
+  graph.addEdge("27", "37",48.64 );
+  graph.addEdge("28", "31",85.70 );
+  graph.addEdge("28", "36",25.02 );
+  graph.addEdge("31", "32", 76.48);
+  graph.addEdge("32", "33", 52.58);
+  graph.addEdge("32", "41",31.37 );
+  graph.addEdge("36", "35",30.38 );
+  graph.addEdge("36", "37",40.00 );
+  graph.addEdge("35", "39", 36.57);
+  graph.addEdge("35", "34", 25.96);
+  graph.addEdge("34", "38", 36.10);
+  graph.addEdge("38", "39", 24.28);
+  graph.addEdge("38", "41", 17.09);
+  graph.addEdge("41", "42", 10.39);
+  graph.addEdge("39", "40",27.06 );
+  graph.addEdge("40", "49",16.52 );
+  graph.addEdge("40", "44",22.83 );
+  graph.addEdge("42", "43", 33.00);
+  graph.addEdge("49", "37", 14.59);
+  graph.addEdge("49", "50",43.66 );
+  graph.addEdge("44", "48", 18.04);
+  graph.addEdge("44", "43",12.50 );
+  graph.addEdge("37", "63", 72.95);
+  graph.addEdge("43", "45", 19.30);
+  graph.addEdge("45", "46", 11.12);
+  graph.addEdge("45", "51", 35.74);
+  graph.addEdge("45", "47",20.56 );
+  graph.addEdge("47", "48", 5.76);
+  graph.addEdge("47", "65",30.35 );
+  graph.addEdge("47", "62",44.38 );
+  graph.addEdge("48", "50",15.90 );
+  graph.addEdge("50", "65",28.22 );
+  graph.addEdge("65", "66", 4.63);
+  graph.addEdge("65", "64",21.62 );
+  graph.addEdge("65", "68", 31.90);
+  graph.addEdge("66", "69",31.63 );
+  graph.addEdge("69", "70",40.94 );
+  graph.addEdge("62", "51", 26.60);
+  graph.addEdge("62", "70",  34.34);
+  graph.addEdge("64", "63", 7.64);
+  graph.addEdge("64", "67", 8.11);
+ //graph.addEdge("67", "63", ); 없는거 같음
+  graph.addEdge("80", "81", 4.26);
+  graph.addEdge("67", "73",108.18 );
+  graph.addEdge("73", "84", 26.50);
+  graph.addEdge("73", "74", 24.41);
+  graph.addEdge("84", "85",5.41 );
+  graph.addEdge("84", "81",27.96 );
+  graph.addEdge("82", "83", 4.00);
+  graph.addEdge("81", "82", 11.11);
+  graph.addEdge("51", "61", 40.55);
+  graph.addEdge("51", "33",57.08 );
+  graph.addEdge("33", "52", 9.38);
+  graph.addEdge("52", "53",57.38 );
+  graph.addEdge("53", "55", 33.09);
+  graph.addEdge("53", "54", 33.62);
+  graph.addEdge("55", "56", 32.00);
+  graph.addEdge("55", "59",31.99 );
+  graph.addEdge("54", "56",29.94 );
+  graph.addEdge("56", "57", 28.69);
+  graph.addEdge("54", "60", 15.74);
+  graph.addEdge("59", "57",34.97 );
+  graph.addEdge("57", "58", 5.27);
+  graph.addEdge("59", "61", 19.85);
+  graph.addEdge("61", "71", 49.03);
+  graph.addEdge("71", "70", 35.26);
+  graph.addEdge("70", "72", 42.89);
+  graph.addEdge("71", "86",31.87 );
+  //graph.addEdge("72", "86",24.92 );
+  graph.addEdge("77", "78", 36.49);
+  graph.addEdge("79", "78", 7.49);
+  graph.addEdge("78", "77",25.79 );
+  graph.addEdge("76", "75",  25.84);
+  graph.addEdge("75", "74", 15.00);
+  graph.addEdge("75", "77", 82.00);
 
     path = graph.Dijkstra(start, finish);
     var linePath=[];
 
     for (i = 0; i<path.length; i++ ){
         console.log(path[i]); 
-        //A C D F E
-        //집 마트 초등학교 역 놀이터
 
         for (j=0; j<vertexData.length; j++){
             if (vertexData[j][0]==path[i]){//최단 경로를 이루는 정점의 좌표
@@ -220,8 +436,6 @@ function getShortCut(){
             }
         }
     }
-
-    
   
   // 지도에 표시할 선을 생성합니다
   polyline.setMap(null);
@@ -232,6 +446,16 @@ function getShortCut(){
   finishMarker.setPosition(finishPos);
   finishMarker.setMap(map);
   
+   
+
+// 인포윈도우를 생성합니다
+var infowindow = new kakao.maps.InfoWindow({
+  position : finishPos, 
+  content : shortDis+"m<br>"+"도보 약"+shortTime+"분"
+});
+
+// 마커 위에 인포윈도우를 표시합니다. 두번째 파라미터인 marker를 넣어주지 않으면 지도 위에 표시됩니다
+infowindow.open(map, finishMarker); 
 
 }
 
@@ -269,7 +493,7 @@ function startClock(){
             user.setMap(null); 
             user.setMap(map); //지도에 원 표시
 
-            map.panTo(userPos);
+            //map.panTo(userPos);
 
         }, function(error) {
             console.error(error);
