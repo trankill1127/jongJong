77265380f04f3239b9c59a923c6190c88d10b196
seunghyun0522 <cvtcvt007@naver.com>
2024-01-29 19:16:35 +0900

    지도 띄우기

--- src/controllers/serviceController.js
+++ src/controllers/serviceController.js
@@ -2,5 +2,8 @@ export const main = (req, res) => {
   return res.render("main", { pageTitle: "main" });
 };
 export const service = (req, res) => {
-  return res.render("service", { pageTitle: "service" });
+  return res.render("service", {
+    pageTitle: "service",
+    KAKAO_MAPS_API_KEY: process.env.KAKAO_MAPS_API_KEY,
+  });
 };
